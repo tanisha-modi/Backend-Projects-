582c786dc1bb01ab36a100681a10585083b90f0b
Tanisha <tanishamodi12@gmail.com>
2024-03-11 17:28:58 +0530

    learnt

---
+++ dataModelling/mongo/models/hospital-management/patient.models.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const patientSchmea = new mongoose.Schema({
+    name :{
+        type: String,
+        required : true,
+    },
+    diagonsedWith:{
+        type: String,
+        required : true
+    },
+    address:{
+        type: String,
+        required : true
+    },
+    age:{
+        type: String,
+        required : true
+    },
+    bloodGroup :{
+        type: String,
+        required : true
+    }, 
+    gender:{
+        type : String, 
+        enum : ["M", "F", "O"],
+        required : true
+    }, 
+    admittedIn:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Hospital"
+    }
+},{timestamps : true});
+
+export const Patient = mongoose.model("Patient", patientSchmea)
